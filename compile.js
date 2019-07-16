ff5d84b3ccc25c6a6ab4a4ca152b85944ace3be9
abhishektanwar <abhishektanwar084@gmail.com>
2019-07-17 00:43:34 +0530

    contract

---
+++ compile.js
@@ -0,0 +1,17 @@
+// we nned to read to solidity contract in order to compile
+// but we cannot do -> require('./contracts/index.sol')
+// because js will try to execute it , and it will throw an error 
+// as it index.sol is in solidity so we need to read the index.sol file first.
+
+const path = require('path'); //it will be used to create a path from compile.js to index.sol
+// path modules guarantee cross platform compatibility
+const fs = require('fs');
+const solc = require('solc');//solidity compiler
+const inboxPath = path.resolve(__dirname , 'contracts' ,'Inbox.sol');
+// __dirname gives the path to base directory then arguments are give in '' separated by , for destinamtion file/folder
+
+const source = fs.readFileSync(inboxPath , 'utf8');
+//console.log(solc.compile(source,1)); //here 1 is no. of contracts being compiled
+// after compiling the contract we need other files to use the compiled file so we export this
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const  { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'picture west maple inmate govern basket provide absurd tomorrow service exchange situate',
+    'https://rinkeby.infura.io/v3/aea3778c75fe4ac7bc71b96e1f770d6b'
+);
+// if we want to deploy using remix , Web3 provider, run ganache at 
+// localhost:8545/7545
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+
+// this is deploying a contract using infura(rinkeby)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('attempting to deploy from contract' , accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data:'ox' +  bytecode, arguments:['hi there']})
+        .send({from : accounts[0]});
+
+    console.log('contract deployed to  ' ,result.options.address);
+};
+deploy();
